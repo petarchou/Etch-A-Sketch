809c980f92da60f0cb0d195bfa4a2a67fda3fb89
Petar Petrov <ppetrov12@abv.bg>
2022-10-16 20:22:15 +0300

    Add clear board button & resize board slider. Change visuals

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ const grid = document.querySelector('.grid-container');
 const gridBlock = document.createElement('div');
 gridBlock.classList.add('grid-block');
 
-let size = 100;
-gridBlock.style.width = (100/size) + 'vw';
-gridBlock.style.height = (90/size) + 'vh';
+let size = 20;
+gridBlock.style.width = (100/size) + '%';
+gridBlock.style.height = (100/size) + '%';
+
 
 for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
@@ -26,14 +27,47 @@ document.body.onmouseup = function (event) {
 
 
 const gridElements = document.querySelectorAll('.grid-block');
-
-const changeBG = event => event.target.style.backgroundColor = 'red';
+const changeBG = event => event.target.style.backgroundColor = 'rgba('+Math.random()*256+","+Math.random()*256+","+Math.random()*256+")";
 
 function addListeners(element) {
     element.addEventListener('mouseover',
         event1 => {
         if(mouseDown[0] ===1)changeBG(event1)
         });
+    element.addEventListener('mousedown', event =>
+    {if(event.buttons===1)changeBG(event)});
+}
+gridElements.forEach(addListeners);
+
+//Generate Slider Value (used for grid size)
+let slider = document.getElementById('grid-slider');
+let sliderVal = document.getElementById('range-value');
+sliderVal.innerHTML = slider.value;
+slider.oninput = function () {
+    sliderVal.innerHTML = slider.value;
+}
+//Change grid when slider is released
+slider.onmouseup = function () {
+    createGrid(slider.value);
 }
 
-gridElements.forEach(addListeners);
+
+//Clear Board Button
+const button = document.querySelector('.grid-button');
+button.onclick = () => createGrid(size);
+//Clear Board func
+function createGrid(newSize) {
+    const gridElements = document.querySelectorAll('.grid-block');
+    gridElements.forEach(element => element.remove());
+    gridBlock.style.width = (100/newSize) + '%';
+    gridBlock.style.height = (100/newSize) + '%';
+
+
+    for (let i = 0; i < newSize; i++) {
+        for (let j = 0; j < newSize; j++) {
+            grid.appendChild(gridBlock.cloneNode(true));
+        }
+    }
+    const newElements = document.querySelectorAll('.grid-block');
+    newElements.forEach(addListeners);
+}
